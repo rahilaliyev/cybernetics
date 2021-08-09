c4b1bdaaa9dcf714aa626bf718b90d16b473a80b
Rahil <rahil.eliyev@gmail.com>
2021-08-10 01:03:11 +0400

    scroll navbar

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const ul = document.querySelector("nav ul");
 hamburgerMenu.addEventListener("click", () => {
   ul.classList.toggle("slide-out");
 });
+
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 100) {
+    document.querySelector("body nav").classList.add("active");
+  } else {
+    document.querySelector("body nav").classList.remove("active");
+  }
+});
